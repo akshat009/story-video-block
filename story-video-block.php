4482e345692b8ce98b94d95de66d5de6218d1b6d
Akshat <saxena.akshat.akshat@gmail.com>
2026-08-05 16:18:44 +0530

    Set plugin Author to real name

--- story-video-block.php
+++ story-video-block.php
@@ -5,7 +5,7 @@
  * Version:           0.1.0
  * Requires at least: 6.8
  * Requires PHP:      7.4
- * Author:            akshat009
+ * Author:            Akshat Saxena
  * Author URI:        https://github.com/akshat009
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
